96da0ff58b457c78dc1208852b0b5af8a7d0932f
antonella <antonella.makahe@gmail.com>
2022-10-05 10:51:09 +1100

    adding dark theme switch to the body

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+let isModalOpen = false;
+let contrastToggle = false;
 
-
-
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += " dark-theme"
+  }
+  else {
+    document.body.classList.remove("dark-theme")
+  }
+}
 
 function contact(event) {
   event.preventDefault();
@@ -25,3 +34,15 @@ function contact(event) {
       );
     });
 }
+
+
+
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open")
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
+}
+
